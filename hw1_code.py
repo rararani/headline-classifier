9a35b8d5ec2a6929495be27a4738e688e92bd056
Rajvi Rana <rajvirana707@gmail.com>
2021-09-22 01:21:03 -0400

    properly fitted and transformed date onto vectorizers

--- hw1_code.py
+++ hw1_code.py
@@ -16,18 +16,21 @@ def load_data():
     real = [headline.strip() for headline in file]
     file.close()
 
-    real_vectorizer = CountVectorizer()
-    real_vectorizer = real_vectorizer.fit(real)
-    real = real_vectorizer.fit_transform(real)
-    fake_vectorizer = CountVectorizer()
-    fake_vectorizer = fake_vectorizer.fit(fake)
-    fake = fake_vectorizer.fit_transform(fake)
-
-    # next split the real and fake data sets into training, testing, and validation sets
     real_train, real_leftover = train_test_split(real, train_size=0.7)
     real_test, real_val = train_test_split(real_leftover, train_size=0.5)
-    fake_train, fake_leftoever = train_test_split(fake, train_size=0.7)
-    fake_test, fake_val = train_test_split(fake_leftoever, train_size=0.5)
+    fake_train, fake_leftover = train_test_split(fake, train_size=0.7)
+    fake_test, fake_val = train_test_split(fake_leftover, train_size=0.5)
+
+    vectorizer = CountVectorizer()
+    real_train = vectorizer.fit_transform(real_train)
+    real_test = vectorizer.transform(real_test)
+    real_val = vectorizer.transform(real_val)
+
+    vectorizer1 = CountVectorizer()
+    fake_train = vectorizer1.fit_transform(fake_train)
+    fake_test = vectorizer1.transform(fake_test)
+    fake_val = vectorizer1.transform(fake_val)
+
 
     return real_train, real_test, real_val, fake_train, fake_test, fake_val
 
